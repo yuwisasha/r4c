ad01827091e99ce7513bcd6706e0c862f8c9f43e
sanek <alexandr.panteleev2000@gmail.com>
2023-09-25 18:21:19 +0300

    Task1 done

--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.core import serializers
+from django.http import HttpRequest, JsonResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Robot
+
+
+@require_POST
+@csrf_exempt
+def create_robot(request: HttpRequest) -> JsonResponse:
+    """Create a Robot database instace by given JSON"""
+
+    data = json.loads(request.body.decode("utf-8"))
+    model = data.get("model")
+    version = data.get("version")
+    robot = Robot(serial=f"{model}-{version}", **data)
+    robot.clean_fields()
+    robot.save()
+    serialized_robot = serializers.serialize("json", [robot])
+    return JsonResponse(json.loads(serialized_robot)[0]["fields"])
